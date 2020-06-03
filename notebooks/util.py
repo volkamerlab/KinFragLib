4de4cce3392f90bc263906fae530261ce101d509
Unknown <dominique.sydow@posteo.de>
2020-06-03 13:55:35 +0200

    Extend fragment library reading (e.g. read w/wo dummy, 3D ROMol)
    Affects _read_subpocket_fragments() and read_fragment_library()

--- notebooks/util.py
+++ notebooks/util.py
@@ -27,7 +27,7 @@ SUBPOCKET_COLORS = {
     'X': 'grey'
 }
 
-def read_fragment_library(path_to_lib, remove_dummy=True):
+def read_fragment_library(path_to_lib):
     """
     Read fragment library from sdf files (one file per subpocket).
     
@@ -35,8 +35,6 @@ def read_fragment_library(path_to_lib, remove_dummy=True):
     ----------
     path_to_lib : str
         Path to fragment library folder.
-    remove_dummy : bool
-        Replace dummy atoms with hydrogens in fragments (default), or leave dummy atoms in fragments.
     
     
     Returns
@@ -52,11 +50,11 @@ def read_fragment_library(path_to_lib, remove_dummy=True):
     # iterate over subpockets
     for subpocket in subpockets:
 
-    	data[subpocket] = _read_subpocket_fragments(subpocket, path_to_lib, remove_dummy)
+    	data[subpocket] = _read_subpocket_fragments(subpocket, path_to_lib)
         
     return data
 
-def _read_subpocket_fragments(subpocket, path_to_lib, remove_dummy=True):
+def _read_subpocket_fragments(subpocket, path_to_lib):
     """
     Read fragments for input subpocket.
     
@@ -66,8 +64,6 @@ def _read_subpocket_fragments(subpocket, path_to_lib, remove_dummy=True):
         Subpocket name, i.e. AP, SE, FP, GA, B1, or B2.
     path_to_lib : str
         Path to fragment library folder.
-    remove_dummy : bool
-        Replace dummy atoms with hydrogens in fragments (default), or leave dummy atoms in fragments.
     
     Returns
     -------
@@ -79,47 +75,52 @@ def _read_subpocket_fragments(subpocket, path_to_lib, remove_dummy=True):
         
     data = []
 
-    for mol_raw in mol_supplier:
+    for mol in mol_supplier:
         
-        if remove_dummy:
+        # Replace dummy atoms with hydrogens in fragments
+        dummy = Chem.MolFromSmiles('*')
+        hydrogen = Chem.MolFromSmiles('[H]', sanitize=False)
+        mol_wo_dummy = AllChem.ReplaceSubstructs(mol, dummy, hydrogen, replaceAll=True)[0]
         
-            # Replace dummy atoms with hydrogens in fragments
-            dummy = Chem.MolFromSmiles('*')
-            hydrogen = Chem.MolFromSmiles('[H]', sanitize=False)
-            mol = AllChem.ReplaceSubstructs(mol_raw, dummy, hydrogen, replaceAll=True)[0]
-            mol = Chem.RemoveHs(mol)  # Remove all hydrogens but explicit hydrogens
-            
-        else:
-            
-            mol = Chem.RemoveHs(mol_raw)
+        # Remove all hydrogens but explicit hydrogens
+        mol_wo_dummy = Chem.RemoveHs(mol_wo_dummy)      
+        mol_w_dummy = Chem.RemoveHs(mol)
         
         # Generate SMILES
-        smiles = Chem.MolToSmiles(mol)
+        smiles_wo_dummy = Chem.MolToSmiles(mol_wo_dummy)
+        smiles_w_dummy = Chem.MolToSmiles(mol_w_dummy)
         
         # 2D coordinates
-        AllChem.Compute2DCoords(mol)
+        AllChem.Compute2DCoords(mol_wo_dummy)
+        AllChem.Compute2DCoords(mol_w_dummy)
         
         # Add property information stored for each fragment, e.g. kinase group
         data.append(
             [
+                mol_wo_dummy,
+                mol_w_dummy,
                 mol,
-                mol.GetProp('kinase'),
-                mol.GetProp('family'),
-                mol.GetProp('group'),
-                mol.GetProp('complex_pdb'),
-                mol.GetProp('ligand_pdb'),
-                mol.GetProp('alt'),
-                mol.GetProp('chain'),
-                mol.GetProp('atom.prop.subpocket'),
-                mol.GetProp('atom.prop.environment'),
-                smiles
+                mol_w_dummy.GetProp('kinase'),
+                mol_w_dummy.GetProp('family'),
+                mol_w_dummy.GetProp('group'),
+                mol_w_dummy.GetProp('complex_pdb'),
+                mol_w_dummy.GetProp('ligand_pdb'),
+                mol_w_dummy.GetProp('alt'),
+                mol_w_dummy.GetProp('chain'),
+                mol_w_dummy.GetProp('atom.prop.subpocket'),
+                mol_w_dummy.GetProp('atom.prop.environment'),
+                smiles_wo_dummy,
+                smiles_w_dummy
             ]
         )
-
-    return pd.DataFrame(
+        
+    fragment_library = pd.DataFrame(
         data,
-        columns='ROMol kinase family group complex_pdb ligand_pdb alt chain atom_subpockets atom_environments smiles'.split()
+        columns='ROMol ROMol_dummy ROMol_original kinase family group complex_pdb ligand_pdb alt chain atom_subpockets atom_environments smiles smiles_dummy'.split()
     )
+    fragment_library['subpocket'] = subpocket
+
+    return fragment_library
 
 
 def get_original_ligands(fragment_library_concat):
