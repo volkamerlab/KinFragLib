b99b366b050c7899092e8eada6432e72d12efe85
dominiquesydow <dominique.sydow@posteo.de>
2020-06-11 08:54:06 +0200

    Remove AP fragments w/o aromatic rings

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -886,7 +886,7 @@ def plot_fragment_descriptors(descriptors):
     plt.savefig(f'figures/descriptors.png', dpi=300)
     
     
-def draw_selected_fragments(selected_fragments, fragments, mols_per_row=3):
+def draw_selected_fragments(selected_fragments, fragments, mols_per_row=3, max_mols=50):
     """
     Draw fragments selected by complex and ligand PDB ID.
     
@@ -898,6 +898,8 @@ def draw_selected_fragments(selected_fragments, fragments, mols_per_row=3):
         Fragments (including data like complex and ligand PDB ID, chain ID, and alternate model).
     mols_per_row : 
         Number of molecules per row.
+    max_mols : int
+        Number of molecules displayed.
         
     Returns
     -------
@@ -921,12 +923,12 @@ def draw_selected_fragments(selected_fragments, fragments, mols_per_row=3):
     )
 
     # Draw selected fragments
-    image = draw_fragments(selected_fragments, mols_per_row)
+    image = draw_fragments(selected_fragments, mols_per_row, max_mols)
     
     return image
 
 
-def draw_fragments(fragments, mols_per_row=10):
+def draw_fragments(fragments, mols_per_row=10, max_mols=50):
     """
     Draw fragments.
     
@@ -934,8 +936,10 @@ def draw_fragments(fragments, mols_per_row=10):
     ----------
     fragments : pandas.DataFrame
         Fragments (including data like complex and ligand PDB ID, chain ID, and alternate model).
-    mols_per_row : 
+    mols_per_row : int
         Number of molecules per row.
+    max_mols : int
+        Number of molecules displayed.
 
     Returns
     -------
@@ -944,9 +948,9 @@ def draw_fragments(fragments, mols_per_row=10):
     """
 
     image = Draw.MolsToGridImage(
-        fragments.ROMol, 
-        maxMols=200,
+        fragments.ROMol,
         molsPerRow=mols_per_row, 
+        maxMols=max_mols,
         legends=fragments.apply(
             lambda x: f'{x.complex_pdb}|{x.chain}:{x.ligand_pdb}' if x.alt == ' ' else f'{x.complex_pdb}|{x.chain}|{x.alt}:{x.ligand_pdb}',
             axis=1
@@ -956,7 +960,7 @@ def draw_fragments(fragments, mols_per_row=10):
     return image
 
 
-def draw_ligands_from_pdb_ids(complex_pdbs, ligand_pdbs, sub_img_size=(150, 150), mols_per_row=1):
+def draw_ligands_from_pdb_ids(complex_pdbs, ligand_pdbs, sub_img_size=(150, 150), mols_per_row=1, max_mols=50):
     """
     Draw ligands from PDB ID (fetch data directly from KLIFS database).
     
@@ -970,6 +974,8 @@ def draw_ligands_from_pdb_ids(complex_pdbs, ligand_pdbs, sub_img_size=(150, 150)
         Image size.
     mols_per_row : 
         Number of molecules per row.
+    max_mols : int
+        Number of molecules displayed.
 
     Returns
     -------
@@ -1044,7 +1050,8 @@ def draw_ligands_from_pdb_ids(complex_pdbs, ligand_pdbs, sub_img_size=(150, 150)
         mols,
         subImgSize=sub_img_size,
         legends=legends,
-        molsPerRow=mols_per_row
+        molsPerRow=mols_per_row,
+        maxMols=max_mols
     )
     
     return image
