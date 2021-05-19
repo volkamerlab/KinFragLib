c215b8c6608f2c2864c11a4420c5a5d7a015958f
Unknown <dominique.sydow@posteo.de>
2021-05-19 11:43:26 +0200

    Make KinFragLib repo pip-installable

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name="kinfraglib",
+    version="1.0.0",
+    description="Kinase-focused fragment library",
+    url="https://github.com/volkamerlab/kinfraglib",
+    author="Volkamer Lab",
+    author_email="andrea.volkamer@charite.de",
+    license="MIT",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Unix",
+    ],
+)
