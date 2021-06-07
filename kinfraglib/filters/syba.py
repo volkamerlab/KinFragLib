56387d94cd8bd57b0e54ca157263cfcfe2f35af7
NonaR <rothkugelsonja@aol.de>
2021-06-07 15:38:42 +0200

    add syba calculation function

--- kinfraglib/filters/syba.py
+++ kinfraglib/filters/syba.py
@@ -1,3 +1,26 @@
 """
 Contains the SYBA filter.
 """
+from syba.syba import SybaClassifier
+
+def calc_syba(smiles_series):
+    """
+    Go through SMILES Series ans calculate the SYnthetic Bayesian Accessibility.
+    
+    Parameters
+    ----------
+    smiles_series : pandas.Series
+        smiles series containing fragment smiles strings
+    
+    
+    Returns
+    -------
+    ??array??
+        SYBA value for each SMILES
+    """
+    sybas = []
+    syba = SybaClassifier()
+    syba.fitDefaultScore()
+    for smiles in smiles_series:
+        sybas.append(syba.predict(smiles))
+    return sybas
