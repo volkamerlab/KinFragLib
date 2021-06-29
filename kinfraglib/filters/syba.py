c1c039ccb5c0309a5c9de9bf95c8fd901f34417c
Sonja Rothkugel <rothkugelsonja@aol.de>
2021-06-29 13:29:43 +0200

    changes in docstring, changes in function, changes in return object
    changes in function to avoid to need using a loop for calling the function; returns a dictionary with the different dataframes/dicts for better readability in notebook

--- kinfraglib/filters/syba.py
+++ kinfraglib/filters/syba.py
@@ -2,24 +2,48 @@
 Contains the SYBA filter.
 """
 from syba.syba import SybaClassifier
+from . import check
+import pandas as pd
 
-def calc_syba(smiles_series):
+def calc_syba(fragment_library, cutoff=0, cutoff_criteria = "<", column_name = "bool_syba"):
     """
     Go through SMILES Series and calculate the SYnthetic Bayesian Accessibility.
     
     Parameters
     ----------
-    smiles_series : pandas.Series
+    fragment_library : dict
         smiles series containing fragment smiles strings
-    
+    cutoff : int
+        defining the cutoff value for rejecting/accepting fragments
+    cutoff_criteria : str
+        defining if the fragments values need to be >, <, >=, <=, == or != compared to the cutoff_value
+    column_name : str
+        defining the column name where the bool if the fragment is accepted (1) or rejected (0) is stored
     Returns
+    dict
+        Containing
+            A pandas.DataFrame with accepted fragments and their information.
+            A pandas.DataFrame with rejected fragments and their information
+            A dict containing a pandas.DataFrame for each subpocket with all fragments and an additional columns defining
+            wether the fragment is accepted (1) or rejected (0)
     -------
-    list of floats
-        SYBA value for each SMILES
+    
     """
     sybas = []
     syba = SybaClassifier()
     syba.fitDefaultScore()
-    for smiles in smiles_series:
-        sybas.append(syba.predict(smiles))
-    return sybas
+    fragment_library_df = pd.concat(fragment_library).reset_index(drop=True)
+    for subpocket in fragment_library.keys():
+        pocketsyba = []
+        fragment_library_df_subpocket = fragment_library_df.loc[fragment_library_df['subpocket'] == subpocket]
+        for smiles in fragment_library_df_subpocket['smiles']:
+            pocketsyba.append(syba.predict(smiles))
+        sybas.append(pocketsyba)
+    
+    df_accepted, df_rejected, fragment_library_bool = check.accepted_rejected(fragment_library, sybas, cutoff, cutoff_criteria, column_name)
+    d = dict()
+    d['df_accepted'] = df_accepted
+    d['df_rejected'] = df_rejected
+    d['fragment_library'] = fragment_library_bool
+    d['sybas'] = sybas
+    return d
