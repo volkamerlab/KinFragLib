90a0069b87f5d03359c4b8f30957cef2ff05e491
Sonja Rothkugel <rothkugelsonja@aol.de>
2021-11-05 18:29:03 +0100

    added first functionality to first pipeline functions

--- kinfraglib/filters/pipeline.py
+++ kinfraglib/filters/pipeline.py
@@ -18,7 +18,7 @@ def start_pipeline(
     general_parameters,
 ):
     if pains_parameters.get('pains_filter'):
-        print("Apply PAINS filter.")
+        print("Apply PAINS filter..")
         pains_dict = filters.pains.get_pains(fragment_library)
         fragment_library = pains_dict["fragment_library"]
         pains_df = pains_dict["pains"]
@@ -37,7 +37,7 @@ def start_pipeline(
             display(num_fragments_pains)
 
     if brenk_parameters.get("brenk_filter"):
-        print("Apply Brenk filter.")
+        print("Apply Brenk filter..")
         DATA_BRENK = brenk_parameters.get("substructure_file")
         brenk_dict = filters.unwanted_substructures.get_brenk(
             fragment_library, DATA_BRENK
@@ -59,7 +59,7 @@ def start_pipeline(
             display(num_fragments_brenk)
 
     if ro3_parameters.get('ro3_filter'):
-        print("Apply Ro3 filter.")
+        print("Apply Ro3 filter..")
         fragment_library = filters.ruleofthree.get_ro3_frags(fragment_library)
 
         if general_parameters.get("show_stats"):
@@ -77,9 +77,10 @@ def start_pipeline(
             display(num_fragments_ro3)
 
     if qed_parameters.get('qed_filter'):
-        print("Apply QED filter.")
+        print("Apply QED filter..")
         cutoff_qed = qed_parameters.get("cutoff_value")
         cutoff_crit_qed = qed_parameters.get("cutoff_crit")
+        plot_stats_qed = qed_parameters.get("plot_stats")
 
         if general_parameters.get("show_stats"):
             fragment_library = filters.qed.get_qed(
@@ -101,15 +102,63 @@ def start_pipeline(
 
         if qed_parameters.get("do_plot"):
             filters.plots.make_hists(
-                fragment_library, "qed", "QED", plot_stats=True, cutoff=cutoff_qed
+                fragment_library, "qed", "QED", plot_stats=plot_stats_qed, cutoff=cutoff_qed
             )
 
     if bb_parameters.get('bb_filter'):
-        print("Apply BB filter.")
+        print("Apply BB filter..")
+        fragment_library = filters.building_blocks.check_building_blocks(
+            fragment_library,
+            bb_parameters.get("bb_file"),
+        )
+        if general_parameters.get("show_stats"):
+            num_fragments_bb = pd.concat(
+                [
+                    filters.analysis.count_fragments(fragment_library, "pre_filtered"),
+                    filters.analysis.count_accepted_rejected(
+                        fragment_library, "bool_bb", "enamine"
+                    ),
+                ],
+                axis=1,
+            )
+            num_fragments_bb.append(num_fragments_bb.sum().rename('Total'))
+            display(num_fragments_bb)
     if syba_parameters.get('syba_filter'):
-        print("Apply SYBA filter.")
-    if qed_parameters.get('retro_filter'):
-        print("Apply pairwise retrosynthesizability filter.")
+        print("Apply SYBA filter..")
+        cutoff_syba = syba_parameters.get("cutoff_value")
+        cutoff_crit_syba = syba_parameters.get("cutoff_crit")
+        query_type = syba_parameters.get("query_type")
+        plot_stats_syba = syba_parameters.get("plot_stats")
+        fragment_library = filters.syba.calc_syba(
+            fragment_library,
+            cutoff=cutoff_syba,
+            cutoff_criteria=cutoff_crit_syba,
+            query_type=query_type,
+        )
+    if general_parameters.get("show_stats"):
+        num_fragments_syba = pd.concat(
+            [
+                filters.analysis.count_fragments(
+                    fragment_library, "pre_filtered"
+                ),
+                filters.analysis.count_accepted_rejected(
+                    fragment_library, "bool_syba", "syba"
+                ),
+            ],
+            axis=1,
+        )
+        num_fragments_syba.append(num_fragments_syba.sum().rename('Total'))
+        display(num_fragments_syba)
+    if syba_parameters.get("do_plot"):
+        filters.plots.make_hists(
+            fragment_library,
+            colname="syba",
+            filtername="SYBA",
+            plot_stats=plot_stats_syba,
+            cutoff=cutoff_syba
+        )
+    if retro_parameters.get('retro_filter'):
+        print("Apply pairwise retrosynthesizability filter..(ToDo)")
 
     return {
         'fragment_library': fragment_library,
