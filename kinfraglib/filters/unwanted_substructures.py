538b770de14e2015c49fe054834ade59cf973ffa
Sonja Rothkugel <rothkugelsonja@aol.de>
2021-11-11 17:53:35 +0100

    unwanted_substructures.py spellcheck and comments

--- kinfraglib/filters/unwanted_substructures.py
+++ kinfraglib/filters/unwanted_substructures.py
@@ -15,14 +15,14 @@ def get_pains(fragment_library):
     Parameters
     ----------
     fragment_library : dict
-        fragments organized in subpockets inculding all information
+        fragments organized in subpockets including all information
 
     Returns
     -------
     fragment_library, matches: tuple(dict,dict)
         Containing
             A dict containing a pandas.DataFrame for each subpocket with all fragments and an
-            additional column (bool_pains) defining wether the fragment is accepted (1) or
+            additional column (bool_pains) defining whether the fragment is accepted (1) or
             rejected (0).
             A pandas.DataFrame with the fragments and the names of the first PAINS structure found
             in the fragment.
@@ -33,7 +33,7 @@ def get_pains(fragment_library):
     params = FilterCatalogParams()
     params.AddCatalog(FilterCatalogParams.FilterCatalogs.PAINS)
     catalog = FilterCatalog(params)
-
+    # save fragment library as DataFrame
     fragment_library_df = pd.concat(fragment_library).reset_index(drop=True)
     # search for PAINS
     matches = []
@@ -55,9 +55,9 @@ def get_pains(fragment_library):
             # collect indices of molecules without PAINS
             clean.append(index)
             accepted_bool.append(1)
-
+    # store fragment and pains structure found in the fragment
     matches = pd.DataFrame(matches)
-
+    # add a boolean column if the fragment contains a pains structure
     fragment_library_bool = building_blocks._add_bool_column(
         fragment_library, accepted_bool, "bool_pains"
     )
@@ -72,9 +72,9 @@ def get_brenk(fragment_library, DATA):
     Parameters
     ----------
     fragment_library : dict
-        fragments organized in subpockets inculding all information
+        fragments organized in subpockets including all information
     DATA : str
-        path to the csv file provided by Brenk
+        path to the csv file provided by Brenk et al.
 
     Returns
     -------
@@ -88,24 +88,30 @@ def get_brenk(fragment_library, DATA):
     """
     # Code adapted from https://github.com/volkamerlab/teachopencadd/blob/master/teachopencadd/talktorials/T003_compound_unwanted_substructures/talktorial.ipynb # noqa: E501
 
+    # read in csv file with unwanted substructure molecules
     substructures = pd.read_csv(DATA / "unwanted_substructures.csv", sep=r"\s+")
     substructures["rdkit_molecule"] = substructures.smarts.apply(Chem.MolFromSmarts)
     print(
         "Number of unwanted substructures in Brenk et al. collection:",
         len(substructures),
     )
-
+    # save fragment library as DataFrame
     fragment_library_df = pd.concat(fragment_library).reset_index(drop=True)
-    # search for PAINS
-    matches = []
-    clean = []
-    rejected = []
-    brenk_bool = []
+
+    matches = []        # variable to store the matches (fragment and unwanted substructure found)
+    clean = []          # variable to store the fragment indices without unwanted substructures
+    rejected = []       # variable to store the fragment indices with unwanted substructures
+    brenk_bool = []     # variable to store a bool for each fragment if unwanted substr. was found
+    # iterate through rows of the fragment library Dataframe
     for index, row in fragment_library_df.iterrows():
-        molecule = row.ROMol
+        molecule = row.ROMol        # save molecule of fragment
         match = False
+        # iterate through unwanted substructure molecules
         for _, substructure in substructures.iterrows():
+            # check if the current fragment contains the unwanted substructure
             if molecule.HasSubstructMatch(substructure.rdkit_molecule):
+                # if unwanted substructure is in fragment save fragment, unwanted substructure and
+                # unwanted substructure name
                 matches.append(
                     {
                         "fragment": molecule,
@@ -113,16 +119,16 @@ def get_brenk(fragment_library, DATA):
                         "substructure_name": substructure["name"],
                     }
                 )
-                match = True
-        if not match:
+                match = True        # set match to true
+        if not match:       # fragment has no unwanted substructure
             clean.append(index)
             brenk_bool.append(1)
-        else:
+        else:       # unwanted substructure was found in fragment
             brenk_bool.append(0)
             rejected.append(index)
-
+    # add unwanted substructures found to DataFrame
     matches = pd.DataFrame(matches)
-
+    # add boolean column if an unwanted substructure was found to fragment library
     fragment_library_bool = building_blocks._add_bool_column(
         fragment_library, brenk_bool, "bool_brenk"
     )
